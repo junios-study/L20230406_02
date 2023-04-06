2a1e46b27bb18688802300a7e7fa89a2d2da4ab0
junios <junios@naver.com>
2023-04-06 17:37:38 +0900

    STL 추가

--- main.cpp
+++ main.cpp
@@ -1,41 +1,89 @@
 #include <iostream>
 #include "State.h"
 #include "Transition.h"
+#include <vector>
+#include <list>
+#include <map>
+#include <algorithm>
+
+
 
 using namespace std;
 
 int main()
 {
-	FState* State[4];
-	FTransition* Transition[5];
+	vector<FState*> States;
 
-	State[0] = new FState(1, "배회");
-	State[1] = new FState(2, "추격");
-	State[2] = new FState(3, "공격");
-	State[3] = new FState(4, "죽음");
+	States.push_back(new FState(1, "배회"));
+	States.push_back(new FState(2, "추격"));
+	States.push_back(new FState(3, "공격"));
+	States.push_back(new FState(4, "죽음"));
+	States.push_back(new FState(5, "필살기"));
 
-	Transition[0] = new FTransition(1, "적발견", 2);
-	Transition[1] = new FTransition(2, "적놓침", 1);
-	Transition[2] = new FTransition(2, "사정거리접근", 3);
-	Transition[3] = new FTransition(3, "사정거리이탈", 2);
-	Transition[4] = new FTransition(3, "HP없음", 4);
+	for (int i = 0; i < States.size(); ++i)
+	{
+		cout << States[i]->Name << endl;
+	}
+
+	for (auto iter = States.rbegin(); iter != States.rend(); ++iter)
+	{
+		cout << (*iter)->Name << endl;
+	}
+
+	//for (auto iter : States)
+	//{
+	//	cout << iter->Name << endl;
+	//}
+
+	list<FTransition*> Transitions;
+
+	Transitions.push_back(new FTransition(1, "적발견", 2));
+	Transitions.push_back(new FTransition(2, "적놓침", 1));
+	Transitions.push_back(new FTransition(2, "사정거리접근", 3));
+	Transitions.push_back(new FTransition(3, "사정거리이탈", 2));
+	Transitions.push_back(new FTransition(3, "HP없음", 4));
+
+	for (auto iter = Transitions.rbegin(); iter != Transitions.rend();  ++iter)
+	{
+		cout << (*iter)->Condition << endl;
+	}
+
+	//for (auto iter : Transitions)
+	//{
+	//	cout << iter->Condition << endl;
+	//}
+
+	map<int, FTransition*> TransitionsMap;
+
+	TransitionsMap[1] = new FTransition(1, "적발견", 2);
+	TransitionsMap[2] = new FTransition(2, "적놓침", 1);
+	TransitionsMap[3] = new FTransition(2, "사정거리접근", 3);
+	TransitionsMap[4] = new FTransition(3, "사정거리이탈", 2);
+	TransitionsMap[5] = new FTransition(3, "HP없음", 4);
+
+	
+	for (auto& iter : TransitionsMap)
+	{
+		cout << iter.first << endl;
+		cout << iter.second->Condition << endl;
+	}
 
 	int MonsterCurrentState = 3;
 	std::string MonsterCondition = "HP없음";
 	int MonsterNextState;
 
-	for (int i = 0; i < 5; ++i)
+	for (auto Value : Transitions)
 	{
-		if (Transition[i]->CurrentState == MonsterCurrentState &&
-			Transition[i]->Condition == MonsterCondition)
+		if (Value->CurrentState == MonsterCurrentState &&
+			Value->Condition == MonsterCondition)
 		{
-			MonsterNextState = Transition[i]->NextState;
+			MonsterNextState = Value->NextState;
 		}
 	}
 
-	cout << "현재 몬스터의 상태는 " << State[MonsterCurrentState - 1]->Name;
+	cout << "현재 몬스터의 상태는 " << States[MonsterCurrentState - 1]->Name;
 	cout << "이고 조건은  " << MonsterCondition << "을 해서 다음에는 ";
-	cout << "몬스터가  다음에는 " << State[MonsterNextState - 1]->Name ;
+	cout << "몬스터가  다음에는 " << States[MonsterNextState - 1]->Name ;
 	cout << " 행동을 합니다. " << endl;
 
 
